a2cfe6c3f1ffc786240ec0b32f2cdf8aeb50dbb4
Bernard Duggan <bernard@circl.es>
2023-09-25 13:05:07 +1000

    Bump minimum Elixir version to 1.12

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule ExAwsChime.MixProject do
   def project do
     [
       app: :ex_aws_chime,
-      version: "1.0.1",
-      elixir: "~> 1.10",
+      version: "1.1.0",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package()
